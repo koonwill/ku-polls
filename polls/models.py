810cd075a24d3b38fcfd7be7bc65bd159a108084
koonwill <93331101+koonwill@users.noreply.github.com>
2022-09-13 13:13:22 +0700

    Modify views for use two method #14  and  redirect to polls index page if voting is not allowed #16

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('end date', null=True)
+    end_date = models.DateTimeField('end date', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
@@ -23,13 +23,17 @@ class Question(models.Model):
 
     def is_published(self):
         """return True if current date is on or after question’s publication date."""
-        return timezone.localtime() >= self.pub_date
+        now = timezone.localtime()
+        return now >= self.pub_date
 
     def can_vote(self):
         """check user in the voting period or not then 
         returns True if voting is allowed for this question
         """
-        return self.pub_date <= timezone.localtime() <= self.end_date
+        now = timezone.localtime()
+        if self.end_date == None:
+            return self.pub_date <= now
+        return self.pub_date <= now <= self.end_date
 
 
 class Choice(models.Model):
