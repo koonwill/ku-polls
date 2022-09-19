d3652c565c93e8e57d4c8bdd3130dff19aec220b
koonwill <93331101+koonwill@users.noreply.github.com>
2022-09-20 00:11:05 +0700

    Implement method to check if user voted this question before or not #28 and only authentic can vote #29

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 """Views for Polls Application"""
 from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -8,7 +8,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Choice, Question
+from .models import Choice, Question, Vote
 
 
 class IndexView(generic.ListView):
@@ -41,6 +41,8 @@ class DetailView(LoginRequiredMixin, generic.DetailView):
 
     def get(self, request, pk):
         """Handle of get request to return correct response to detail view."""
+        if not request.user.is_authenticated:
+            return redirect(to='http://127.0.0.1:8000/accounts/login')
         try:
             question = Question.objects.get(pk=pk)
         except Question.DoesNotExist:
@@ -58,10 +60,11 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-@login_required(login_url='/accounts/login')
+@login_required
 def vote(request, question_id):
     """Vote function for voting button"""
     question = get_object_or_404(Question, pk=question_id)
+    user = request.user
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -70,10 +73,10 @@ def vote(request, question_id):
             'question': question,
             'error_message': "You didn't select a choice.",
         })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    try:
+        vote = Vote.objects.get(user=user)
+        vote.choice = selected_choice
+        vote.save()
+    except Vote.DoesNotExist:
+        Vote.objects.create(user=user, choice=selected_choice).save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
